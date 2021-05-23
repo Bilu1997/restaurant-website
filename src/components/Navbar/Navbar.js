a1f019e24ade6714c6bd36487fddeca120a518a5
Bilu1997 <bilubiznes@gmail.com>
2021-05-23 21:09:56 +0200

    Added logo, changed class names

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,27 +20,33 @@ const Navbar = () => {
   return (
     <>
       <nav className={colorChange ? "navbar colorChange" : "navbar"}>
-        <div className="mobile-icon" onClick={handleClick}>
+        <div className="logo__container">
+          <img
+            src={require("../../images/logo.png").default}
+            className="nav__logo"
+          />
+        </div>
+        <div className="mobile__icon" onClick={handleClick}>
           {open ? <FaTimes /> : <FaBars />}
         </div>
-        <ul className={open ? "mobile--menu" : "nav--menu"}>
+        <ul className={open ? "mobile__menu" : "nav__menu"}>
           <li>
-            <a className={open ? "nav-links--mobile" : "nav-links"} href="/">
+            <a className={open ? "nav__links--mobile" : "nav__links"} href="/">
               O nas
             </a>
           </li>
           <li>
-            <a className={open ? "nav-links--mobile" : "nav-links"} href="/">
+            <a className={open ? "nav__links--mobile" : "nav__links"} href="/">
               Menu
             </a>
           </li>
           <li>
-            <a className={open ? "nav-links--mobile" : "nav-links"} href="/">
+            <a className={open ? "nav__links--mobile" : "nav__links"} href="/">
               Promocje
             </a>
           </li>
           <li>
-            <a className={open ? "nav-links--mobile" : "nav-links"} href="/">
+            <a className={open ? "nav__links--mobile" : "nav__links"} href="/">
               Kontakt
             </a>
           </li>
