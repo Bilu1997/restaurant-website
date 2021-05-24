771f26adc4a3f7b3e9821cf155b6bbaaef2e74fc
Bilu1997 <bilubiznes@gmail.com>
2021-05-24 15:19:09 +0200

    Added text, button on hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,6 +5,15 @@ const Hero = () => {
   return (
     <>
       <div className="background__container">
+        <div className="hero__wrapper">
+          <h1 className="hero__h1">Pizzeria Kuchcik</h1>
+          <p className="hero__p">
+            Z pasji do smacznej kuchni! Zapoznaj się z naszym menu!
+          </p>
+          <button to="/menu" className="hero__button">
+            MENU
+          </button>
+        </div>
         <img className="background__image" />
       </div>
     </>
